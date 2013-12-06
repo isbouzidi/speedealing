181bc41a494ab853f4e93a89667d71f0103efdbb
Herve Prot <herve.prot@symeos.com>
2013-12-06 12:51:11 +0100

    Fix angular:

--- nodejs/public/js/speedealing/controllers/europexpress.js
+++ nodejs/public/js/speedealing/controllers/europexpress.js
@@ -1,10 +1,13 @@
-angular.module('mean.europexpress').controller('EEPlanningController', ['$scope', '$routeParams', '$location', 'Global', 'EEPlanning', function($scope, $routeParams, $location, Global, Object) {
+angular.module('mean.europexpress').controller('EEPlanningController', ['$scope', '$routeParams', '$location', '$route', 'Global', 'EEPlanning', function($scope, $routeParams, $location, $route, Global, Object) {
 		$scope.global = Global;
 		$scope.showEdit = {};
-		
+
 		$scope.cpt = 0;
 
 		$scope.find = function() {
+			if($routeParams.id1 == null)
+				return $scope.today();
+			
 			//console.log($routeParams);
 			Object.query({week: $routeParams.id1, year: $routeParams.id2}, function(tournees) {
 				$scope.tournees = tournees;
@@ -16,20 +19,99 @@ angular.module('mean.europexpress').controller('EEPlanningController', ['$scope'
 			$scope.showEdit[id] = true;
 		};
 
-		$scope.isCollapsed = false;
-
-		/*$scope.week = function() {
+		$scope.today = function() {
 			var d = new Date();
 			d.setHours(0, 0, 0);
 			d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-			return Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7).toString() + '/' + d.getFullYear();
-		};*/
+			var week = Math.ceil((((d - new Date(d.getFullYear(), 0, 1)) / 8.64e7) + 1) / 7).toString();
+			$location.path('europexpress/planning/' + week + '/' + d.getFullYear());
+		};
+
+		$scope.next = function() {
+			var year = parseInt($routeParams.id2);
+			var week = parseInt($routeParams.id1)
+			if (week === 52) {
+				year++;
+				week = 0;
+			}
+			week++;
+			
+			console.log('europexpress/planning/' + week + '/' + year);
+
+			$location.path('europexpress/planning/' + week + '/' + year);
+		};
 		
+		$scope.previous = function() {
+			var year = parseInt($routeParams.id2);
+			var week = parseInt($routeParams.id1)
+			if (week === 1) {
+				year--;
+				week = 53;
+			}
+			week--;
+
+			$location.path('europexpress/planning/' + week + '/' + year);
+		};
+
 		$scope.week = $routeParams.id1 + '/' + $routeParams.id2;
 
 		$scope.disableEdit = function() {
-			for(var i in $scope.showEdit)
+			for (var i in $scope.showEdit)
 				$scope.showEdit[i] = false;
+		};
+
+		$scope.driverAutoCompleteEditor = {
+			minLength: 1,
+			dataTextField: "name",
+			dataValueField: "id",
+			filter: "contains",
+			dataSource: {
+				serverFiltering: true,
+				serverPaging: true,
+				pageSize: 5,
+				transport: {
+					read: {
+						url: "api/user/name/autocomplete",
+						type: "POST",
+						dataType: "json"
+					}
+				}
+			}
+		};
+
+		$scope.sousTraitantAutoCompleteEditor = {
+			minLength: 1,
+			dataTextField: "name",
+			filter: "contains",
+			dataSource: {
+				serverFiltering: true,
+				serverPaging: true,
+				pageSize: 5,
+				transport: {
+					read: {
+						url: "api/societe/autocomplete?fournisseur=SUBCONTRACTOR",
+						type: "POST",
+						dataType: "json"
+					}
+				}
+			}
+		};
+
+		$scope.update = function(id) {
+			var article = $scope.aday;
+
+			article.$update(function() {
+				$route.reload();
+				//$location.path('articles/' + article._id);
+			});
+		};
+
+		$scope.findOne = function(id) {
+			Object.get({
+				planningId: id
+			}, function(aday) {
+				$scope.aday = aday;
+			});
 		};
 
 		/* $scope.create = function() {
@@ -53,18 +135,6 @@ angular.module('mean.europexpress').controller('EEPlanningController', ['$scope'
 		 $scope.articles.splice(i, 1);
 		 }
 		 }
-		 };
-		 
-		 $scope.update = function() {
-		 var article = $scope.article;
-		 if (!article.updated) {
-		 article.updated = [];
-		 }
-		 article.updated.push(new Date().getTime());
-		 
-		 article.$update(function() {
-		 $location.path('articles/' + article._id);
-		 });
 		 };
 		 
 		 $scope.find = function() {
--- nodejs/public/js/speedealing/services/europexpress.js
+++ nodejs/public/js/speedealing/services/europexpress.js
@@ -1,7 +1,7 @@
 //Articles service used for REST endpoint
 angular.module('mean.europexpress').factory("EEPlanning", ['$resource', function($resource) {
-    return $resource('api/europexpress/planning/:year/:week/:Id', {
-        Id: '@_id'
+    return $resource('api/europexpress/planning/:year/:week/:planningId', {
+        planningId: '@_id'
     }, {
         update: {
             method: 'PUT'
