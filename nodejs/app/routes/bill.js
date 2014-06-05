8ba3a07024ed89e0e62452793b6319fca1b81ff0
Herve Prot <herve.prot@symeos.com>
2014-06-05 16:53:57 +0200

    Fix error pdf bill generated

--- nodejs/app/routes/bill.js
+++ nodejs/app/routes/bill.js
@@ -175,9 +175,9 @@ Object.prototype = {
 		bill = _.extend(bill, req.body);
 
 		bill.save(function(err, doc) {
-			if(err)
+			if (err)
 				console.log(err);
-			
+
 			//console.log(doc);
 			res.json(doc);
 		});
@@ -300,8 +300,8 @@ Object.prototype = {
 				//console.log(doc);
 
 				var tab_latex = "";
-				for (var i = 0; i < doc.lines.length; i++){
-					tab_latex += doc.lines[i].product.name.substring(0,11).replace(/_/g, "\\_") + "&\\specialcell[t]{\\textbf{" + doc.lines[i].product.label + "}\\\\" +doc.lines[i].description.replace(/\n/g, "\\\\").replace(/%/g, "\%") + "\\\\}&" + doc.lines[i].tva_tx + "\\% &" + latex.price(doc.lines[i].pu_ht) + "&" + doc.lines[i].qty + "&" + latex.price(doc.lines[i].total_ht) + "\\tabularnewline\n";
+				for (var i = 0; i < doc.lines.length; i++) {
+					tab_latex += doc.lines[i].product.name.substring(0, 11).replace(/_/g, "\\_") + "&\\specialcell[t]{\\textbf{" + doc.lines[i].product.label + "}\\\\" + doc.lines[i].description.replace(/\n/g, "\\\\").replace(/%/g, "\%").replace(/&/g, "\&") + "\\\\}&" + doc.lines[i].tva_tx + "\\% &" + latex.price(doc.lines[i].pu_ht) + "&" + doc.lines[i].qty + "&" + latex.price(doc.lines[i].total_ht) + "\\tabularnewline\n";
 				}
 				//console.log(products)
 
