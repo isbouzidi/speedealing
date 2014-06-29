40e1ae6912aaae78e1c2892124162bac5f4aa35d
Herve Prot <herve.prot@symeos.com>
2014-06-30 01:46:43 +0200

    Modify dict

--- htdocs/societe/class/societe.class.php
+++ htdocs/societe/class/societe.class.php
@@ -1747,7 +1747,7 @@ class Societe extends nosqlDocument {
 				$params = array('group' => true);
 				//$result = $this->getView("count_status", $params);
 				$result = $this->mongodb->aggregate(array(
-					array('$match' => array('Status' => array('$ne' => 'ST_NEVER'))),
+					array('$match' => array('Status' => array('$nin' => array('ST_NEVER','ST_NO')))),
 					array('$project' => array(
 							"Status" => 1
 						)),
@@ -1757,7 +1757,7 @@ class Societe extends nosqlDocument {
 				//$params = array('group' => true, "startkey" => array($user->name), "endkey" => array($user->name, new stdClass()));
 				//$result = $this->getView("commercial_status", $params);
 				$result = $this->mongodb->aggregate(array(
-					array('$match' => array('commercial_id.id' => $user->login, 'Status' => array('$ne' => 'ST_NEVER'))),
+					array('$match' => array('commercial_id.id' => $user->login, 'Status' => array('$nin' => array('ST_NEVER','ST_NO')))),
 					array('$project' => array(
 							"Status" => 1
 						)),
@@ -1785,17 +1785,35 @@ class Societe extends nosqlDocument {
 					$label = $langs->trans($aRow->_id);
 				}
 
-				if ($i == 0) { // first element
-					$output[$i] = new stdClass();
-					$output[$i]->name = $label;
-					$output[$i]->y = $aRow->count;
-					$output[$i]->sliced = true;
-					$output[$i]->selected = true;
-				} else
-					$output[$i] = array($label, $aRow->count);
+				//if ($i == 0) { // first element
+				//	$output[$i] = new stdClass();
+				//	$output[$i]->name = $label;
+				//	$output[$i]->y = $aRow->count;
+				//	$output[$i]->sliced = true;
+				//	$output[$i]->selected = true;
+				//} else
+
+				$output[$i] = new stdClass();
+				$output[$i]->order = $this->fk_extrafields->fields->Status->values->$key->order;
+				$output[$i]->value = array($label, $aRow->count);
 				$i++;
 			}
-			return $output;
+
+			function cmp($a, $b) {
+				return ($a->order > $b->order);
+			}
+			
+			usort($output, "cmp");
+
+			$i = 0;
+			foreach ($output as $aRow) {
+				error_log(print_r($aRow, true));
+
+				$output_final[$i] = $aRow->value;
+				$i++;
+			}
+
+			return $output_final;
 		} else {
 			$total = 0;
 			$i = 0;
