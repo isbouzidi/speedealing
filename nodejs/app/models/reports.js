09f51fb2c156f042cab032ae491897dd73892ce0
Bouzidi Issam <bouzidissam@gmail.com>
2014-07-18 16:57:05 +0200

    add report model

--- nodejs/app/models/reports.js
+++ nodejs/app/models/reports.js
@@ -5,19 +5,45 @@
  */
 var mongoose = require('mongoose'),
                 config = require('../../config/config'),
-		gridfs = require('../controllers/gridfs'),
 		Schema = mongoose.Schema,
 		i18n = require("i18next"),
-		timestamps = require('mongoose-timestamp'),
-		crypto = require('crypto'),
-		authTypes = ['github', 'twitter', 'facebook', 'google'];
-
+		timestamps = require('mongoose-timestamp');
+		
 /**
  * Report Schema
  */
 
 var ReportSchema = new Schema({
-    
+        model: String,
+	dateReport: Date,	
+	societe: {
+		_id: {type: Schema.Types.ObjectId},
+		name: String
+	},
+	duration: Number,
+	contacts:[{
+            _id: Schema.Types.ObjectId,
+            name: String,
+            poste: String
+            }],
+	products:[{
+            id:Schema.Types.ObjectId,
+            name:String
+            }],
+	actions:[{
+            type: String,
+            method: String,
+            date: Date,
+            realised : Boolean
+	}],
+	optional : Schema.Types.Mixed,
+	comments: String,
+	author:{
+            _id: String,
+            name: String
+    }
 });
+
+ReportSchema.plugin(timestamps);
 
 mongoose.model('report', ReportSchema, 'Reports');
