bfb33d640519ac2c8f3098f29066fe6fe5d2265b
Bouzidi Issam <bouzidissam@gmail.com>
2014-07-18 16:43:18 +0200

    choose a contact(without creating new one)

--- nodejs/app/routes/reports.js
+++ nodejs/app/routes/reports.js
@@ -7,6 +7,32 @@ var mongoose = require('mongoose'),
 		gridfs = require('../controllers/gridfs'),
 		config = require('../../config/config');
 
+var ContactModel = mongoose.model('contact');
+
 module.exports = function(app, passport, auth) {
     
+    app.post('/api/report/autocomplete', auth.requiresLogin, function(req, res){
+            
+            if (req.body.val === null)
+                return res.send(200, {});
+            console.log("contact : " + req.body.val);
+            var val = req.body.val;
+            
+            var query = { "$or": [
+                            {firstname: new RegExp(val, "i")},
+                            {lastname: new RegExp(val, "i")}
+                        ]
+            };
+            
+            ContactModel.find(query, {}, {limit: 5}, function(err, doc){
+                if(err){
+                    console.log(err);
+                    return;
+                }
+                
+                return res.send(200, doc);
+                
+            });
+            
+	});
 };
--- nodejs/public/js/speedealing/controllers/reports.js
+++ nodejs/public/js/speedealing/controllers/reports.js
@@ -1,56 +1,92 @@
 angular.module('mean.reports').controller('ReportController', ['$scope', '$location', '$http', '$routeParams', '$modal', '$filter', '$upload', '$timeout', 'pageTitle', 'Global', 'Reports', function($scope, $location, $http, $routeParams, $modal, $filter, $upload, $timeout, pageTitle, Global, Reports) {
-    
+
         $scope.global = Global;
-}]);
+    }]);
 
 angular.module('mean.reports').controller('ReportCreateController', ['$scope', '$http', '$modalInstance', '$modal', '$upload', '$route', 'Global', 'Reports', function($scope, $http, $modalInstance, $modal, $upload, $route, Global, Reports) {
 
-    $scope.global = Global;
-    $scope.report = {}
-    $scope.init = function(){
-        $scope.items = [{
-                id: 'DISCOVERY',
-                name: 'Découverte'},
-            {
-                id: 'PRE-SIGN',
-                name: 'Pré-Signature'},
-            {
-                id: 'Contract',
-                name: 'Suivi/Contrat Signé'
-            }];   
-    };
-    
-    $scope.addNewContact = function() {
+        $scope.global = Global;
+        $scope.report = {};
+        $scope.report.contacts = [];
         
+        
+        $scope.init = function() {
+            $scope.items = [{
+                    id: 'DISCOVERY',
+                    name: 'Découverte'},
+                {
+                    id: 'PRE-SIGN',
+                    name: 'Pré-Signature'},
+                {
+                    id: 'Contract',
+                    name: 'Suivi/Contrat Signé'
+                }];
+            
+        };
+
+        $scope.addNewContact = function() {
+
             var modalInstance = $modal.open({
-                    templateUrl: '/partials/contacts/create.html',
-                    controller: "ContactCreateController",
-                    windowClass: "steps"
+                templateUrl: '/partials/contacts/create.html',
+                controller: "ContactCreateController",
+                windowClass: "steps"
             });
 
             modalInstance.result.then(function(contacts) {
-                    $scope.contacts.push(contacts);
-                    $scope.countContact++;
+                $scope.contacts.push(contacts);
+                $scope.countContact++;
             }, function() {
             });
-    };
-    
-    $scope.updateReportTemplate = function(){
-        
-        var category = $scope.report.category.id;
-        
-        switch(category){
-            case 'DISCOVERY' :
-                $scope.report.template = "/partials/reports/discovery.html";
-                break;
-            case 'PRE-SIGN':
-                $scope.report.template = "/partials/reports/pre-sign.html";
-                break;
-            case 'Contract':
-                $scope.report.template = "/partials/reports/contract.html";
-                break;
-            default : 
-                $scope.report.template = "";
         };
-    };
-}]);
+
+        $scope.updateReportTemplate = function() {
+
+            var category = $scope.report.category.id;
+
+            switch (category) {
+                case 'DISCOVERY' :
+                    $scope.report.template = "/partials/reports/discovery.html";
+                    break;
+                case 'PRE-SIGN':
+                    $scope.report.template = "/partials/reports/pre-sign.html";
+                    break;
+                case 'Contract':
+                    $scope.report.template = "/partials/reports/contract.html";
+                    break;
+                default :
+                    $scope.report.template = "";
+            }
+            ;
+        };
+
+        $scope.contactAutoComplete = function(val) {
+            
+            return $http.post('api/report/autocomplete', {
+                val: val
+            }).then(function(res) {
+                //console.log(res.data);
+
+                return res.data;
+            });
+
+        };
+        
+        $scope.addContact = function(){
+            
+            var add = {
+                    id: $scope.report.contact._id, 
+                    name: $scope.report.contact.name, 
+                    poste: $scope.report.contact.poste
+                };
+            
+            $scope.report.contacts.push(add);
+            $scope.report.contact = "";
+            console.log($scope.report);
+        };
+        
+        $scope.delete = function($index){
+            
+            $scope.report.contacts.splice($index, 1);
+        };
+            
+    }]);
