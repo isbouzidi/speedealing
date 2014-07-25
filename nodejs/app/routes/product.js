9ddebaf9b996932f466aa30e7a2fe8519ac650e3
Herve Prot <herve.prot@symeos.com>
2014-07-25 07:11:10 +0200

    Fix barcode search product

--- nodejs/app/routes/product.js
+++ nodejs/app/routes/product.js
@@ -426,7 +426,7 @@ Object.prototype = {
 			query.type = req.query.type;
 
 		if (req.query.barCode)
-			query.barCode = {$ne: null};
+			query.barCode = {$nin:[null, ""]};
 
 		ProductModel.find(query, "ref label barCode billingMode type caFamily", {limit: 50}, function(err, products) {
 			if (err)
