4ed8bf45b36eaf2a95b4303b77bd31c6aa984dca
Herve Prot <herve.prot@symeos.com>
2014-07-30 14:40:32 +0200

    Add import contact

--- nodejs/app/routes/societe.js
+++ nodejs/app/routes/societe.js
@@ -1126,7 +1126,7 @@ module.exports = function(app, passport, auth) {
 
 		var convertRow = function(tab, row, index, cb) {
 			var contact = {
-				Status : "ST_ENABLE",
+				Status: "ST_ENABLE",
 				tag: []
 			};
 			contact.country_id = "FR";
@@ -1278,19 +1278,19 @@ module.exports = function(app, passport, auth) {
 										//console.log(data);
 
 										//console.log(row[10]);
-										console.log(contact);
+										//console.log(contact);
 										//console.log(societe.datec);
 
-										/*	societe.save(function(err, doc) {
-										 if (err)
-										 console.log(err);
-										 /*if (doc == null)
-										 console.log("null");
-										 else
-										 console.log(doc);*/
+										contact.save(function(err, doc) {
+											if (err)
+												console.log(err);
+											/*if (doc == null)
+											 console.log("null");
+											 else
+											 console.log(doc);*/
 
-										callback();
-										//});
+											callback();
+										});
 									});
 
 								});
