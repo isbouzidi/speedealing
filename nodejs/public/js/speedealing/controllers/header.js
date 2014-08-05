1bd6ba67a586d66d652b9d8718da95104b2ef717
Bouzidi Issam <bouzidissam@gmail.com>
2014-08-05 12:35:53 +0200

    modify search engine

--- nodejs/public/js/speedealing/controllers/header.js
+++ nodejs/public/js/speedealing/controllers/header.js
@@ -1,57 +1,62 @@
 angular.module('mean.system').controller('HeaderController', ['$scope', '$http', '$route', 'Global', 'pageTitle', function($scope, $http, $route, Global, pageTitle) {
-		$scope.global = Global;
-		//console.log(Global);
-		
-		$scope.title = pageTitle.getTitle();
-                $scope.currentPage = 0;
-                $scope.pageSize = 5;
-                
-                $scope.withMenu = function() {
-			//console.log(Global);
-			if (Global && Global.user.right_menu)
-				return "with-menu";
-		};
+        $scope.global = Global;
+        //console.log(Global);
 
-		$scope.getEntities = function() {
-			$http({method: 'GET', url: 'api/entity/select'
-			}).
-					success(function(data, status) {
-				$scope.entities = data;
-			});
-		};
+        $scope.title = pageTitle.getTitle();
 
-		$scope.entity = {id: Global.user.entity, name: Global.user.entity};
+        $scope.withMenu = function() {
+            //console.log(Global);
+            if (Global && Global.user.right_menu)
+                return "with-menu";
+        };
 
-		$scope.changeEntity = function() {
-			$scope.title = pageTitle.getTitle();
-			//Global.user.entity = $scope.entity.id;
-			$route.reload();
-		};
-                
-                $scope.$watch('searchItem', function(item){
+        $scope.getEntities = function() {
+            $http({method: 'GET', url: 'api/entity/select'
+            }).
+                    success(function(data, status) {
+                        $scope.entities = data;
+                    });
+        };
+
+        $scope.entity = {id: Global.user.entity, name: Global.user.entity};
+
+        $scope.changeEntity = function() {
+            $scope.title = pageTitle.getTitle();
+            //Global.user.entity = $scope.entity.id;
+            $route.reload();
+        };
+
+        $scope.filteredResults = [];
+        $scope.currentPage = 1;
+        $scope.numPerPage = 3;
+        $scope.maxSize = 5;
+        
+        $scope.$watch('searchItem', function(item) {
+            $scope.currentPage = 1;
+            if (item) {
+
+                $http({method: 'GET', url: '/api/contact/searchEngine',
+                    params: {item: item}
+
+                }).success(function(data) {
+
+                    $scope.results = data;
+                    $scope.resultsCount = data.length;
+            
+                    $scope.$watch('currentPage + numPerPage', function() {
+                        var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+                        var end = begin + $scope.numPerPage;
+
+                        $scope.filteredResults = $scope.results.slice(begin, end);
+                    });
                     
-                    if(item){
-                        
-                        $http({ method: 'GET', url: '/api/contact/searchEngine', 
-                                params: { item: item }
-
-                                }).success(function(data) {
-
-                                    $scope.results = data;
-                         });
-                         
-                        $scope.numberOfPages=function(){
-                            return Math.ceil($scope.results.length/$scope.pageSize);          
-                        };
-                        
-                    }
                 });
+                
+                $scope.numPages = function() {
+                    return Math.ceil($scope.results.length / $scope.numPerPage);
+                };
 
-	}]);
-    
-        app.filter('startFrom', function() {
-            return function(input, start) {
-                start = +start;
-                return input.slice(start);
-            };
+            }
         });
+
+    }]);
