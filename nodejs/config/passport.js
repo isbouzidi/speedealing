07c738e47b4525c0aa2b77f4319559b50fc02072
Regis Houssin <regis.houssin@capnetworks.com>
2014-11-19 07:51:39 +0000

    Fix: avoid warning

--- nodejs/config/passport.js
+++ nodejs/config/passport.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var mongoose = require('mongoose'),
 		_ = require('lodash'),
 		LocalStrategy = require('passport-local').Strategy,
--- nodejs/config/routes.js
+++ nodejs/config/routes.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var async = require('async'),
 		fs = require('fs'),
 		modules = require('../app/controllers/modules'),
