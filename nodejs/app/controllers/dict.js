902b1eef69e3a6a728e25bace424f13503d2bc4a
Regis Houssin <regis.houssin@capnetworks.com>
2014-11-20 17:36:50 +0100

    Revert "Fix: add conf loader TODO à finaliser"
    This reverts commit 4d23f1e6f2d49fb8a1adf9c76e3cef534c121a43.

--- nodejs/app/controllers/dict.js
+++ nodejs/app/controllers/dict.js
@@ -9,16 +9,6 @@ var mongoose = require('mongoose'),
 		i18n = require("i18next"),
 		async = require("async");
 
-/* Conf */
-var confSchema = new mongoose.Schema({
-	_id: String,
-	entity: String,
-	values: {type: mongoose.Schema.Types.Mixed}
-});
-
-var ConfModel = mongoose.model('conf', confSchema, 'Conf');
-
-/* Extrafields */
 var extrafieldSchema = new mongoose.Schema({
 	_id: String,
 	ico: String,
@@ -36,7 +26,6 @@ var extrafieldSchema = new mongoose.Schema({
 
 var ExtrafieldModel = mongoose.model('extrafields', extrafieldSchema, 'ExtraFields');
 
-/* Dict */
 var dictSchema = new mongoose.Schema({
 	_id: String,
 	lang: String,
@@ -50,7 +39,6 @@ var DictModel = mongoose.model('dict', dictSchema, 'Dict');
 
 
 /* Public declaration methods. See definition for documentation. */
-exports.conf = readConf;
 exports.dict = readDict;
 exports.extrafield = readExtrafield;
 
@@ -92,9 +80,6 @@ function convertDict(params, doc, callback) {
 	callback(result);
 }
 
-function readConf(params, callback) {
-	// TODO complete this
-}
 
 function readDict(params, callback) {
 
