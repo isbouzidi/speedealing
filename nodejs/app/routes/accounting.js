3888235bd2e9ed6b9e663a6da26a535dec3a5b52
Herve Prot <herve.prot@symeos.com>
2014-11-20 17:33:26 +0100

    Fix bug account

--- nodejs/app/routes/accounting.js
+++ nodejs/app/routes/accounting.js
@@ -26,7 +26,7 @@ Dict.dict({dictName: "fk_tva", object: true}, function (err, docs) {
 });
 
 var globalConst = {};
-Dict.conf({dictName: "const", object: true}, function (err, doc) {
+Dict.dict({dictName: "const", object: true}, function (err, doc) {
 	globalConst = doc.values;
 	console.log(doc);
 });
