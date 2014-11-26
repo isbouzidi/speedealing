130dc88194d69b4b11bc55204f27d5e4a724203a
Herve Prot <herve.prot@symeos.com>
2014-11-26 22:55:09 +0100

    import contact

--- nodejs/app/routes/contacts.js
+++ nodejs/app/routes/contacts.js
@@ -710,7 +710,7 @@ Contact.prototype = {
 
 		ContactModel.find({Tag: "Arseg"}, function (err, contacts) {
 			//console.log(contact);
-			json2csv({data: contacts, fields: ['_id', 'firstname', 'lastname', 'societe', 'poste', 'address', 'zip', 'town', 'phone', 'phone_mobile', 'email', 'Tag'], del: ";"}, function (err, csv) {
+			json2csv({data: contacts, fields: ['_id', 'firstname', 'lastname', 'societe.name', 'poste', 'address', 'zip', 'town', 'phone', 'phone_mobile', 'email', 'Tag'], del: ";"}, function (err, csv) {
 				if (err)
 					console.log(err);
 
