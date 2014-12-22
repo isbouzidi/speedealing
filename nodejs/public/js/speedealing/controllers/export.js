da1c63ffe4478ae70a67ad67a1e90521194cef95
Bouzidi Issam <bouzidissam@gmail.com>
2014-12-22 15:59:56 +0100

    update export csv

--- nodejs/public/js/speedealing/controllers/export.js
+++ nodejs/public/js/speedealing/controllers/export.js
@@ -1,5 +1,5 @@
 angular.module('mean.export').controller('ExportCSVController', ['$scope', '$routeParams', '$location', '$route', '$modal', '$timeout', '$http', '$filter', '$upload', 'pageTitle', 'Global', 'Export', function ($scope, $routeParams, $location, $route, $modal, $timeout, $http, $filter, $upload, pageTitle, Global, Export) {
-        
+
         $scope.global = Global;
         $scope.gridData = [{Result: null}];
         $scope.msg = {
@@ -8,21 +8,26 @@ angular.module('mean.export').controller('ExportCSVController', ['$scope', '$rou
             color: null,
             text: null
         };
-        
-        $scope.init = function(){
-            
+
+        $scope.init = function () {
+
+            $scope.collections = [
+                {id: 'contact', name: 'contact'},
+                {id: 'societe', name: 'societe'}
+            ];
         };
-        
+
         $scope.colDefs = [];
-        
-        $scope.$watch('gridData', function() {
-           
+
+        $scope.$watch('gridData', function () {
+
             $scope.colDefs = [];
             var i = 0;
-            angular.forEach(Object.keys($scope.gridData[0]), function(key){
-                
-                $scope.colDefs.push({ field: key });                
+            angular.forEach(Object.keys($scope.gridData[0]), function (key) {
+
+                $scope.colDefs.push({field: key});
             });
+
             $scope.count = $scope.gridData.length;
         });
 
@@ -32,50 +37,56 @@ angular.module('mean.export').controller('ExportCSVController', ['$scope', '$rou
             columnDefs: 'colDefs',
             enableColumnResize: false
         };
-        
-        $scope.getData = function () {
-                        
-            var str = $scope.request;            
-            var col = str.substring(0,str.indexOf("."));
-            str = str.replace(col, "db");
-            
-            //if($scope.global.user.rights[col] && $scope.global.user.rights[col].export){
-            if(true){
-                var p = {
-                    request: str,
-                    model: col
-                };
 
-                Export.query(p, function (data) {
-                    
-                    $scope.gridData = data[0];    
-                    $scope.csvData = data[1];
-                    $scope.count = $scope.gridData.length;
-                    $scope.msg={
-                        enable: true,
-                        icon: "icon-tick",
-                        color: "green-gradient",
-                        text: "Success !"
+        $scope.getData = function () {
+
+            if (typeof $scope.collection !== 'undefined') {
+                var str = "db." + $scope.request;
+
+                if (true) {
+                    var p = {
+                        request: str,
+                        model: $scope.collection
                     };
-                }, function(error){
-                    $scope.msg={
+
+                    Export.query(p, function (data) {
+
+                        $scope.gridData = data[0];
+                        $scope.csvData = data[1];
+                        $scope.count = $scope.gridData.length;
+                        $scope.msg = {
+                            enable: true,
+                            icon: "icon-tick",
+                            color: "green-gradient",
+                            text: "Success !"
+                        };
+                    }, function (error) {
+                        $scope.msg = {
+                            enable: true,
+                            icon: "icon-cross",
+                            color: "red-gradient",
+                            text: error.data
+                        };
+                    });
+                } else {
+                    $scope.msg = {
                         enable: true,
                         icon: "icon-cross",
                         color: "red-gradient",
-                        text: error.data
+                        text: "Vous n'avez pas l'autorisation !"
                     };
-                });
+                }
             }else{
-                $scope.msg={
+                $scope.msg = {
                     enable: true,
                     icon: "icon-cross",
                     color: "red-gradient",
-                    text: "Vous n'avez pas l'autorisation !"
+                    text: "Vous n'avez pas choisi une collection !"
                 };
-            }                                  
+            }
         };
-        
-        $scope.export = function(){
+
+        $scope.export = function () {
             var element = angular.element('<a/>');
             element.attr({
                 href: 'data:attachment/csv;charset=utf-8,' + encodeURI($scope.csvData),
